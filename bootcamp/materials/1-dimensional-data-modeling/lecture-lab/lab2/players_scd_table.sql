5ba6ac34d0395b1caae7e8da75e8486837f90397
Eldad <eldado@cynamics.ai>
2024-11-27 17:20:12 +0200

    q3+q4 done

--- bootcamp/materials/1-dimensional-data-modeling/lecture-lab/lab2/players_scd_table.sql
+++ bootcamp/materials/1-dimensional-data-modeling/lecture-lab/lab2/players_scd_table.sql
@@ -1,10 +1,10 @@
-create table players_scd_table
+create table players_scd
 (
 	player_name text,
 	scoring_class scoring_class,
 	is_active boolean,
-	start_season integer,
+	start_date integer,
 	end_date integer,
-	current_season INTEGER
+	current_year INTEGER,
+    PRIMARY KEY(player_name, start_date)
 );
-
